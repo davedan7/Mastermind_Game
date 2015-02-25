6045ab0c753ba0b76dec2f0e808b13a139c8b9cc
David Daniel <davedan7@gmail.com>
2015-02-25 11:49:44 -0700

    A few working tests, and methods for the color_checker working

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,12 +1,72 @@
 require_relative 'response'
+# evaluate
+# Write methods
+# Output
+# Logic
+
 
 class Mastermind
+	attr_reader :secret
+	def initialize
+		@secret = "BBGB"
+	end
+
   def execute(input)
-    secret = "BBGB"
+    # secret = "BBGB"
     if input == secret
       Response.new(:message => "You Win!", :status => :won)
     else
       Response.new(:message => "Guess again!", :status => :continue)
     end
   end
+
+	def evaluate_guess(guess)
+		# count = 0
+		# until count == 10
+			guess = guess.chars
+			answer = @secret.chars
+			common = (guess & answer)
+			num = common.length
+			puts "There are #{num} colors in common: #{common} "
+		# end
+		
+	end
+
+	def correct_elements(guess)
+		guess.upcase!
+		# answer = @secret.chars
+		# correct_elements = guess.chars{|char| answer.include?(char)}
+		# print correct_elements
+	end
+
+	def correct_position(guess)
+		check = @secret.chars
+		correct_position = guess.chars.zip(check)
+		num = correct_position.count do |char|
+			char[0]==char[1]
+		end
+		num 
+	end
+
+	def secret  # Works
+	#	scrambled = []
+		colors = ["R", "G", "B", "Y"]
+		scrambled = colors.shuffle!
+		@secret = scrambled.join("") #un-comment this to randomize secret
+
+		# colors = ["R", "G", "B", "Y"]
+		# colors.length.times do 
+		# 	colors.shuffle!
+		# 	scrambled << colors[0]
+		# end
+	end
+
+		#generate a random sequence of colors
+	def guess_evaluator
+	end
+
 end
+
+m = Mastermind.new
+m.correct_position("GGGG")  #BBGB
+# m.evaluate_guess("BBBB")
--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -1,15 +1,45 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/mastermind'
-
+# Have multiple test files? Guess check, logic check, etc, test by class
 class MastermindTest < Minitest::Test
+	attr_reader :mm
+
+	def setup
+		@mm = Mastermind.new
+	end
+
   def test_it_exists
     assert MastermindTest
   end
 
-  def test_it_wins
-    mm = Mastermind.new
-    result = mm.execute("BBGB")
-    assert result.message.downcase.include?("win")
-  end
+	def test_it_produces_a_secret_number_and_stores_as_a_string
+		assert mm.secret.class == String
+	end
+
+	def test_it_produces_a_new_secret_each_new_game #There's a small chance these actually will be the same and the test will fail
+		other_game = Mastermind.new
+		assert other_game.secret != mm.secret
+	end
+
+	def test_it_counts_number_of_characters_in_the_correct_position  # secret = "BBGB" for this test and similar, I assigend mm.secret to a set string. Acutal implementation will be random
+		assert_equal mm.correct_position("BBGB"), 4
+		assert_equal mm.correct_position("BBBB"), 3
+		assert_equal mm.correct_position("RRRR"), 0
+	end
+
+	def test_it_checks_if_guess_equals_secret
+	end
+
+	def test_it_returns_which_guesses_are_correct  		
+		mm.evaluate_guess("GGGG")  # mm.secret = "BBGB"
+	end
+
+
+
+  # def test_it_wins
+  #   mm = Mastermind.new
+  #   result = mm.execute("BBGB")
+  #   assert result.message.downcase.include?("win")
+  # end
 end
