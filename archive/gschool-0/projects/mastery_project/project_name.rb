dd2e60be8d7954a265799f839516b7e0167db5b3
David Daniel <davedan7@gmail.com>
2015-03-02 10:37:12 -0700

    Deleted files' git init git status

--- archive/gschool-0/projects/mastery_project/project_name.rb
+++
@@ -1,8 +0,0 @@
-{
-  :developers => ["First Last 1", "First Last 2"]
-  :project_name => "The Project Name",
-  :project_description => "One paragraph about what the app will do and why",
-  :backend_technologies => ['riak'],
-  :frontend_technologies => ['D3']
-  :repo => 'http://github.com/username/repo_name'
-}
